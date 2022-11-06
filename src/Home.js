ab965509309c38ca2e54e528707061ec19cee19f
MAULIK RAJENDRA PATEL <maulikpatelmow@gmail.com>
2022-11-06 16:53:44 +0530

    services and Trusted components

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,23 @@
 import React from "react";
 import styled from "styled-components";
 import HeroSection from "./components/HeroSection";
+import Services from "./components/Services";
+import Trusted from "./components/Trusted";
 
 const Home = () => {
-  return <HeroSection/>;
+  const data = {
+    name : "thapa Store",
+  }
+
+  return (
+    <>
+  <HeroSection
+  myData={data}
+  />
+  <Trusted/>
+  <Services/>
+  </>
+  );
 };
 
 
