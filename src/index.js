7979d89a9a3d1afdab7dadb61f975a7d6c5d1da4
MAULIK RAJENDRA PATEL <maulikpatelmow@gmail.com>
2022-11-08 11:49:52 +0530

    creating multiple components for grid and list view

--- src/index.js
+++ src/index.js
@@ -4,15 +4,18 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { AppProvider } from "./context/productcontex";
-
+import { FilterContextProvider } from "./context/filter_context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-    <AppProvider>
+  <AppProvider>
+    <FilterContextProvider>
       <App />
-    </AppProvider>
-  );
+    </FilterContextProvider>
+  </AppProvider>
+);
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
